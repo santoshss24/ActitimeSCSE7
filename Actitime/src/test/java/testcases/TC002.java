721138f0c3e9bfc1862fbfe1dcec561eacd3b3ef
Admin <Admin@192.168.2.113>
2021-02-24 19:49:12 +0530

    Changes made to TC002 as requested

--- Actitime/src/test/java/testcases/TC002.java
+++ Actitime/src/test/java/testcases/TC002.java
@@ -2,11 +2,20 @@ package testcases;
 
 import org.testng.annotations.Test;
 
+import pages.homepage;
+
 public class TC002 extends basetest 
 {
 	@Test
 	public void createuser()
 	{
+		homepage page=new homepage(driver, webActionUtil);
+		page.clickOnTask();
+		page.clickOnActionsButton();
+		page.clickOnCopyTaskLink();
+		page.clickOnselectCustomer();
+		
+		
 		
 	}
 
